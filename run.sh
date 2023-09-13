bd58d2eb2bd68e6e007943ba63f8e1c45805d298
Mistyk1 <blotmaxime9@gmail.com>
2023-09-13 20:27:47 +0200

    Revert "truc4"
    This reverts commit 8d0b73e147999723ec48a7eebda6ba4d0d8fb904.

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ if [ -e $JAVA_HOME/jre/lib/javafx.properties ]; then
     javac -d classes */*.java
     java -cp classes Hub
 else
-    javac -d classes */*.java excludesfile QuestionPourUnCarton
+    javac -d classes */*.java excludesfile QuestionPourUnCarton.java
     java -cp classes Hub noJavaFx
 fi
 
