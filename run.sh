aa095bcf8cc3e9e8bd803b31134b45d143109f72
Mistyk1 <blotmaxime9@gmail.com>
2023-09-13 21:02:26 +0200

    truc7

--- run.sh
+++ run.sh
@@ -5,7 +5,11 @@ if [ -e $JAVA_HOME/jre/lib/javafx.properties ]; then
     java -cp classes Hub
 else
     #javac exclude jeux/QuestionPourUnCarton.java -d classes */*.java
-    for %%i in (*.java) do if /i not "%%~i" == "QuestionPourUnCarton.java" javac "%%~i"
+    for %%i in (*.java); do
+        if [ /i not "%%~i" = "QuestionPourUnCarton.java" ]; then
+            javac "%%~i"
+        fi
+    done
     java -cp classes Hub noJavaFx
 fi
 
